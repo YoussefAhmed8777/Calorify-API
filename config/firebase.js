b01959842fa035db41b13b77a15c029cc2d32523
YoussefAhmed8777 <youssefahmedamin28@gmail.com>
2026-02-17 20:02:34 +0200

    initialized and tested firebase configs

---
+++ config/firebase.js
@@ -0,0 +1,11 @@
+const admin = require('firebase-admin');
+const serviceAcc = require('../calorify-firebase-service-account.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAcc),
+});
+
+const auth = admin.auth();
+const db = admin.firestore();
+
+module.exports = {admin, auth, db};
